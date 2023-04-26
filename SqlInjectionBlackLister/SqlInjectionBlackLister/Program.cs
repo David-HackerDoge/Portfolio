4c5d448f2522ef25ee4a2d1cab8a576f369b7a23
divadcrizap <68246591+divadcrizap@users.noreply.github.com>
2023-04-26 12:25:25 -0600

    blacklister
    added sql injection checker and ip blacklister

---
+++ SqlInjectionBlackLister/SqlInjectionBlackLister/Program.cs
@@ -0,0 +1,24 @@
+﻿using SqlInjectionBlackLister;
+
+SqlInjectChecker injectionChecker = new SqlInjectChecker();
+
+string ip = "10.0.0.2";
+string request = "POST /cgi-bin/process.cgi HTTP/1.1\r\nUser-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT) union select password from admin \r\nHost: www.tutorialspoint.com\r\nContent-Type: text/xml; charset=utf-8\r\nContent-Length: length\r\nAccept-Language: en-us\r\nAccept-Encoding: gzip, deflate\r\nConnection: Keep-Alive\r\n\r\n<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<string xmlns=\"http://clearforest.com/\">string</string>";
+string fileName = "blacklist.txt";
+
+bool result = injectionChecker.isInjection(ip, request, fileName);
+if (result == true)
+{
+    Console.WriteLine("Found injection! Added ip to blacklist!");
+    Console.WriteLine("Check logs.txt for more info");
+}
+else if (result == false)
+{
+    Console.WriteLine("All Clear!");
+}
+
+
+
+
+
+
---
+++ SqlInjectionBlackLister/SqlInjectionBlackLister/SqlInjectChecker.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlInjectionBlackLister
+{
+    internal class SqlInjectChecker
+    {
+        void blackListIp(string ip, string filePath)
+        {
+            //create blacklist file if it doesn't exist
+            if (!File.Exists(filePath))
+            {
+                File.WriteAllText(filePath, "");
+            }
+            string[] badips = File.ReadAllLines(filePath);
+            if (badips.Contains(ip))
+            {
+                File.AppendAllText("logs.txt", $"{ip} was already blacklisted. They shouldn't be able to send requests. The blacklist is likely not working\n");
+            }
+            else
+            {
+                File.AppendAllText(filePath, ip + "\n");
+                File.AppendAllText("logs.txt", $"{ip} added to blacklist\n");
+            }
+            
+        }
+
+        /// <summary>
+        /// Checks for sql injection and adds the ip to the blacklist if it finds any
+        /// currently only checks for the keyword select and only checks for all lowercase
+        /// TODO: check using regex, check for other sql terms, check for obfuscation
+        /// note: blacklist is meant to be given to a firewall to prevent additional requests from bad actors
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="request"></param>
+        /// <param name="blackListFilePath"></param>
+        /// <returns></returns>
+        public bool isInjection(string ip, string request, string blackListFilePath)
+        {
+            //create log file if it doesn't exist
+            if (!File.Exists("logs.txt"))
+            {
+                File.WriteAllText("logs.txt", "Log created\n");
+            }
+            File.AppendAllText("logs.txt", $"checking request from {ip} for sql injection\n");
+            if (request.Contains("select"))
+            {
+                File.AppendAllText("logs.txt", $"Sql injection found in request from {ip}!\n");
+                blackListIp(ip, blackListFilePath);
+                return true;
+            }
+            File.AppendAllText("logs.txt", $"nothing unusual was detected in request from {ip}\n");
+            return false;
+        }
+    }
+
+}
+
